e72d4fd5d113c8a8b8de2cc16e69845015d8ef45
bigbang1986 <bigbang1986@foxmail.com>
2015-12-13 23:12:00 +0800

    Update test_coverage.sh

--- test_coverage.sh
+++ test_coverage.sh
@@ -1,3 +1,3 @@
 build/bin/unittests
-lcov --directory  ./oss_sdk/CMakeFiles/osssdkcpp.dir   --capture --output-file app.info  -b oss_sdk/  --no-external
+lcov --directory  ./oss_sdk_cpp/CMakeFiles/osssdkcpp.dir   --capture --output-file app.info  -b oss_sdk_cpp/  --no-external
 genhtml  -o  results  app.info
